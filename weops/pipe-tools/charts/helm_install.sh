ae0a709b4f35a91500c55718c1cf387ca032ae84
zhongming Fan <zhongmingfan@canway.net>
2023-03-06 17:25:37 +0800

    feature: helm install

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -5,9 +5,7 @@ object_versions=("2022", "2019", "2017")
 object=mssql
 
 for version in "${object_versions[@]}"; do
-    version_suffix="$version"
-
-    helm install mssql-$version_suffix --namespace $object -f ./values/${version_suffix}_values.yaml \
-    --set deployment.labels.object_version="$version_suffix" \
+    helm install mssql-$version --namespace $object -f ./values/${version}_values.yaml \
+    --set deployment.labels.object_version="$version" \
     ./mssqlserver-2022
 done
