b20d1ec358ff0e5d4b3c694c6438794a1c9c102a
zhongming Fan <zhongmingfan@canway.net>
2023-03-06 17:55:56 +0800

    feature: helm install

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -5,12 +5,12 @@ object=mssql
 
 helm install mssql-2022 --namespace $object -f ./values/2022_values.yaml \
 --set pod.labels.object_version="2022" \
-./mssqlserver
+./mssqlserver-2022
 
 helm install mssql-2019 --namespace $object -f ./values/2019_values.yaml \
 --set pod.labels.object_version="2019" \
-./mssqlserver
+./mssqlserver-2019
 
 helm install mssql-2017 --namespace $object -f ./values/2017_values.yaml \
 --set pod.labels.object_version="2017" \
-./mssqlserver
+./mssqlserver-2019
