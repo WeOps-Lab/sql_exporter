8569cca329f313635d15731c7ffec94692809dd5
zhongming Fan <zhongmingfan@canway.net>
2023-03-06 16:22:30 +0800

    feature: helm install

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -5,7 +5,7 @@ object_versions=("2022", "2019", "2017")
 object=mssql
 
 for version in "${object_versions[@]}"; do
-    version_suffix="v$version"
+    version_suffix="$version"
 
     helm install mssql-$version_suffix --namespace $object -f ./values/standalone_values.yaml \
     --set image.tag=$version-latest \
