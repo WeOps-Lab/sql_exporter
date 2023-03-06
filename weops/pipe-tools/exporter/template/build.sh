e75cde3bdb9c914971a64d12f301c7badcd99244
zhongming Fan <zhongmingfan@canway.net>
2023-03-06 18:07:53 +0800

    feature: helm install

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for version in 2022; do
+for version in 2022 2019 2017; do
   output_file="standalone_${version}.yaml"
   sed "s/{{VERSION}}/${version}/g" standalone.tpl > ../standalone/${output_file}
 done
