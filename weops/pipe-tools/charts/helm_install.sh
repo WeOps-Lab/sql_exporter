6af5fde15d7582629c9d6a6242e0b3a207076219
zhongming Fan <zhongmingfan@canway.net>
2023-03-06 09:13:52 +0800

    feature: network chaos

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -6,4 +6,5 @@ helm install mssql2022 --namespace mssql \
 --set edition.value=Developer \
 --set sapassword='Weops123!' \
 --set persistence.enabled=false \
+--set deployment.labels.object='mssql' \
 ./mssqlserver-2022
