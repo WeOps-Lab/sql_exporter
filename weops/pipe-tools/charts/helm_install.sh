3f71327b9c50fd2c59b89c9b99589014dae3afa8
zhongming Fan <zhongmingfan@canway.net>
2023-03-13 10:43:18 +0800

    feature: nodeport

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -4,10 +4,10 @@
 object=mssql
 
 helm install mssql-2022 --namespace $object -f ./values/2022_values.yaml ./mssqlserver-2022
-kubectl patch -n $object service mssqlserver-2022 -p '{"spec": {"ports": [{"nodePort": 1433}]}}'
+kubectl patch -n $object service mssqlserver-2022 -p '{"spec": {"ports": [{"NodePort": 1433}]}}'
 
 helm install mssql-2019 --namespace $object -f ./values/2019_values.yaml ./mssqlserver-2019
-kubectl patch -n $object service mssqlserver-2019 -p '{"spec": {"ports": [{"nodePort": 1434}]}}'
+kubectl patch -n $object service mssqlserver-2019 -p '{"spec": {"ports": [{"NodePort": 1434}]}}'
 
 helm install mssql-2017 --namespace $object -f ./values/2017_values.yaml ./mssqlserver-2017
-kubectl patch -n $object service mssql-2017-mssql-linux -p '{"spec": {"ports": [{"nodePort": 1435}]}}'
+kubectl patch -n $object service mssql-2017-mssql-linux -p '{"spec": {"ports": [{"NodePort": 1435}]}}'
