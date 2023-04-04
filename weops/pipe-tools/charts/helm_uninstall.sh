fa605058456058db321c962754d17638519d00d9
zhongming Fan <zhongmingfan@canway.net>
2023-04-04 10:48:05 +0800

    pipeline: helm uninstall

--- weops/pipe-tools/charts/helm_uninstall.sh
+++ weops/pipe-tools/charts/helm_uninstall.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-# 卸载监控对象
-NAMESPACE="mssql"
+# 删除监控对象
+object=mssql
+
+helm uninstall mssql-2022 --namespace $object
+
+helm uninstall mssql-2019 --namespace $object
+
+helm uninstall mssql-2017 --namespace $object
 
 # Uninstall mssql deployments
-for RELEASE in $(helm list -n $NAMESPACE --short)
+for RELEASE in $(helm list -n $object --short)
 do
   echo "Uninstalling $RELEASE ..."
-  helm uninstall -n $NAMESPACE $RELEASE
+  helm uninstall -n $object "$RELEASE"
 done
