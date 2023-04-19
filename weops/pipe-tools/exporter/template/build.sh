c6992259f8aff6cf2a732e493b9823155fb20f50
zhongming Fan <zhongmingfan@canway.net>
2023-04-19 16:37:25 +0800

    deploy: describe for test

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# 测试多对象
 for SQL_OBJECT in mysql postgres oracle mssql; do
   output_file="sql_exporter_${SQL_OBJECT}.yaml"
   sed "s/{{SQL_OBJECT}}/${SQL_OBJECT}/g" standalone.tpl > ../standalone/${output_file}
