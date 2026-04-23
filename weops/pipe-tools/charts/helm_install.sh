23c8f8bb634449f3f1e0ca12c457bd49105b5462
zhongming Fan <zhongmingfan@canway.net>
2026-04-23 10:39:50 +0800

    dp: mssql update EULA acceptance and enhance Helm installation script with bootstrap configuration

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -1,11 +1,51 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # 部署监控对象
 object=mssql
 
-helm install mssql-2022 --namespace $object -f ./values/2022_values.yaml ./mssqlserver-2022
+kubectl get namespace "$object" >/dev/null 2>&1 || kubectl create namespace "$object"
 
-helm install mssql-2019 --namespace $object -f ./values/2019_values.yaml ./mssqlserver-2019
+install_release() {
+  local release=$1
+  local values_file=$2
+  local chart_dir=$3
 
-helm install mssql-2017 --namespace $object -f ./values/2017_values.yaml ./mssqlserver-2017
+  echo ">> deploying ${release}"
+  helm upgrade --install "$release" \
+    --namespace "$object" \
+    --wait \
+    --wait-for-jobs \
+    --timeout 10m \
+    -f "$values_file" \
+    "$chart_dir"
+}
 
+pids=()
+names=()
+
+install_release mssql-2022 ./values/2022_values.yaml ./mssqlserver-2022 &
+pids+=("$!")
+names+=("mssql-2022")
+
+install_release mssql-2019 ./values/2019_values.yaml ./mssqlserver-2019 &
+pids+=("$!")
+names+=("mssql-2019")
+
+install_release mssql-2017 ./values/2017_values.yaml ./mssqlserver-2017 &
+pids+=("$!")
+names+=("mssql-2017")
+
+status=0
+
+for i in "${!pids[@]}"; do
+  if wait "${pids[$i]}"; then
+    echo ">> ${names[$i]} deployed"
+  else
+    echo ">> ${names[$i]} failed" >&2
+    status=1
+  fi
+done
+
+exit "$status"
--- weops/pipe-tools/charts/mssqlserver-2022/templates/_helpers.tpl
+++ weops/pipe-tools/charts/mssqlserver-2022/templates/_helpers.tpl
@@ -23,3 +23,7 @@ If release name contains chart name it will be used as a full name.
 {{- end -}}
 {{- end -}}
 {{- end -}}
+
+{{- define "mssql.serviceName" -}}
+{{- template "mssql.name" . -}}
+{{- end -}}
